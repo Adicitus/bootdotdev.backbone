31e4a37d1cb152fa0b04706cf1f3386e39ae347c
Joakim Olsson <joakim.olsson@adicitus.cloud>
2025-11-14 08:35:52 +0100

    Started implementng public key authentication

--- auth.py
+++ auth.py
@@ -1,17 +1,49 @@
+import os
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import rsa, padding
+from cryptography.hazmat.primitives import serialization, hashes
+from cryptography.exceptions import InvalidSignature
+
+
 import frame
 
-# Super-basic challenge-response setup, to be replaced.
-expected = {
-    "challenge": { "len": 5, "val": b'hello' },
-    "response":  { "val": b'world', "len": 5 }
-}
+
+# TODO: Hard-coded key data, should be moved to dynamic storage:
+server_private_key_b = b'-----BEGIN RSA PRIVATE KEY-----\nMIIEowIBAAKCAQEAvIDEAz0tOPbvVH6519PxDcllVEznZwZbxbY/p8k+y8pSFhO+\nL1TZrwmffS0byTvpdxzJAlIaPZvMUod8luxSd0TGkut6J8REAj6/9ghZdM6CyBx6\nalnViw9w8xr4n95EPJsyeaFPQh+3BoNvW8Ldth7Z5LDSvhc/s+NO7jfwUCHDtsFn\njF3osjnXh6Eg2856WeLzkhpB6RHBOLCINpCnJ2wXA4MZpa/UbzDTycDohhIHLhTl\n99bfHO5A3YjH+HYzxXBg9PezvQwm4GeSyJ/vxCwVaz4v0SEURr6kysyHG0BqwpGc\nPHlaLmd3gMSzGwGCvzAlPic/WXCftCsDkFa/xwIDAQABAoIBAB5EQ0oig6/ZVQfz\n9G1so5BV+CtR0SKa2UJvfFy5n0JugRaiuI9LUvaGwjEsUdanrtldaa2x4XJnS9Er\n3aaIBHJlEcibqaNX9p7bqiExfwJBJFZIMvmqOfOYQ50ChXgkjT/5SrX6ih+GYxys\ndEVqDlpZ2Bz0V/+F+8hWT6FH9xHjyzIuq1sn8lz6VOS7WqzUvHT0XSu4ZYe2MyC0\ngJcn0deImTmVei51fuASc1RPxn+FVSdmUEAEm7cj4ItE2qbxGUZW/sipfA8PY9zC\n8GZWUkHY5IM3OihNGZ1kP8W1qhQzkrHeIDBUkX/eor+QuR+qzoTaM3IbZOeJUFkw\nMA7c2nECgYEA7x1so+JUO9JoO3G/26GOZVNstQDk8gLhzSNQNKeeK9I9l0zpVKgm\nAE1qE42cTS2mQxsjMcVyznHmF3LszI28peuyUJLHAy27E7wPyF5ixtKTIIu2eVlT\nTmutHkaMWR2kHRmXtNdtXLFLFfumuEAS81n5b9Jj9pSUlOdk4NcqTPECgYEAydBo\ntl5+8rvvKc6Vq1Ha0E1OM5Gi64sX7OBpM/f4MeLI1Gk3YWKB9V/j3rQYDND9pyV3\neeQCZPyz9DD0TY2oaOO/MkCiogGq+hantmcK4TI7xG3og1qj70poubEQg5R7Nzb+\n23mcJWiTH47FoMRdgPDNfaAMNhtkpaCqlQojuDcCgYEA4EAxPjWqF/yJoo5Yh8up\nNyAZSFvRW6MWT4Q52VgGAEUJXFdWUP3tvcTcof/iQYN5dWADDs+Fj1XEm+M9b83R\nya4VqnktSa7ez3BFQP48Fwkv0IsXFBGRDm/viQBS6dThsCTrYT/UoeTobXJ4dj4p\nR67vP08KTxwjiM7GjhNc6ZECgYBYq8xGTLCkgDRt/wm902I74at5dwTJTzz85AD1\nR39yk70/rtcZX8nQAQjC4ggrIWxBk3GySZ9PtcRvh07gAFy9cUuhjcqnOepgpbGc\nsBuUpJikDtJ5XErpKZ1n8UdbONMGiJBj6EPWH+N8myN0JrMaozL2fmq/HzwSlcw9\nXc4h8wKBgCtZD8kjipakadSbhosDOTmmnltI7ArMXuPjU/xn30TUJyp6YmHWKSQ5\nRODxzRS0DQhPijNcZmplrB1u4fYo+SapCHiqSSFzRdl3GTLDD9FniFE0nUWw/KUj\nVIdUc/9r1N2yNisZWMMM5uObKNIrFT9YThO8vRf2MRuwJbBp9RWj\n-----END RSA PRIVATE KEY-----\n'
+server_public_key_b = b'-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvIDEAz0tOPbvVH6519Px\nDcllVEznZwZbxbY/p8k+y8pSFhO+L1TZrwmffS0byTvpdxzJAlIaPZvMUod8luxS\nd0TGkut6J8REAj6/9ghZdM6CyBx6alnViw9w8xr4n95EPJsyeaFPQh+3BoNvW8Ld\nth7Z5LDSvhc/s+NO7jfwUCHDtsFnjF3osjnXh6Eg2856WeLzkhpB6RHBOLCINpCn\nJ2wXA4MZpa/UbzDTycDohhIHLhTl99bfHO5A3YjH+HYzxXBg9PezvQwm4GeSyJ/v\nxCwVaz4v0SEURr6kysyHG0BqwpGcPHlaLmd3gMSzGwGCvzAlPic/WXCftCsDkFa/\nxwIDAQAB\n-----END PUBLIC KEY-----\n'
+
+server_private_key   = serialization.load_pem_private_key(
+    server_private_key_b,
+    password=None
+)
+server_public_key   = serialization.load_pem_public_key(server_public_key_b)
+
+# TODO: Client keys should be stored under associated client_id
+client_public_key_b = b'-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyeSYjAhDTqkpFsSEaEIh\nrQzzlJwwE/uaHLbINxBQgWZ6EnxbrQg2jXZTUT7fIa8ImTNg7R/GNxE4Umx6R3Ga\nXpkhoxGhurAOlBUxuQpnPFvApNE5jnyUKtZIO2uv2ToqA1hvJYHTcl1gAUNuEPWW\nGe0fcNKIyKFtuU5LShIpvRW1uH5h4rWqjryVYImF7i9tmvalMYarBYDRq/SoHjz4\n63o56dTqcIJdkYgty7aEw+rmx+Tn6tW/axRw2RH0E3Zb7pAwehJ/HlL/MEfqRiUp\njVMd+kKX6TJhqCAf4QJU+P8/oehDXP89e6C+6AJpRl4DwPn6249PBi8iO5Zavref\nkQIDAQAB\n-----END PUBLIC KEY-----\n'
+client_public_key   = serialization.load_pem_public_key(client_public_key_b)
 
 def challenge(clientsock):
     # This should be replaced with a signature validation.
-    frame.send(clientsock, expected["challenge"]["val"])
+    challenge_data = os.urandom(256)
+
+    frame.send(clientsock, challenge_data)
     response = frame.read(clientsock)
-    if response == expected["response"]["val"]:
+    
+    try:
+        client_public_key.verify(
+            response,
+            challenge_data,
+            padding.PSS(
+                mgf=padding.MGF1(hashes.SHA256()),
+                salt_length=padding.PSS.MAX_LENGTH
+            ),
+            hashes.SHA256()
+        )
         msg = b'Connection authenticated!'
         frame.send(clientsock, msg)
-    else:
-        clientsock.close()
+    except InvalidSignature:
+        connection.close()
+
+def respond(socket):
+    challenge_data = frame.read(socket)
--- client.py
+++ client.py
@@ -1,12 +1,26 @@
 import socket
 
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import padding
+
 import frame
+
+client_private_key_b = b'-----BEGIN RSA PRIVATE KEY-----\nMIIEogIBAAKCAQEAyeSYjAhDTqkpFsSEaEIhrQzzlJwwE/uaHLbINxBQgWZ6Enxb\nrQg2jXZTUT7fIa8ImTNg7R/GNxE4Umx6R3GaXpkhoxGhurAOlBUxuQpnPFvApNE5\njnyUKtZIO2uv2ToqA1hvJYHTcl1gAUNuEPWWGe0fcNKIyKFtuU5LShIpvRW1uH5h\n4rWqjryVYImF7i9tmvalMYarBYDRq/SoHjz463o56dTqcIJdkYgty7aEw+rmx+Tn\n6tW/axRw2RH0E3Zb7pAwehJ/HlL/MEfqRiUpjVMd+kKX6TJhqCAf4QJU+P8/oehD\nXP89e6C+6AJpRl4DwPn6249PBi8iO5ZavrefkQIDAQABAoIBABT21w1RbVXNdH4P\n/XTLCy6qRd/UYRyuF2hsZXlsSQPK1Zm2Tpm23N1PIGMzHV3kzEaGQoYU8tL0h43t\nhDwHdKFmksrZUZsuXXEsizt89E/c8jHbJPgiAQu6sQj0i2/J3aN3yyDQrJl3TzkK\nXLNHoSTDjex7/CajNp7TnbdNEAb5et6rYLUgxW3vsx2c+jEnUgP9po1jO9Xb5KUe\nOeAsoYv0P2PMcihGCRjLKyb4NmHpJGsrqJvrHQNpb4ed9am19p5VlbKn4ENKcLJk\nEBjNrLlsz+SlQJH7113LPnMEPTAFXLdrVNighUucP8tJ6gIWFIK2EsdR+psn8f6M\narrPjo0CgYEA/qiSL/kWlk2l2UvCHC1jxIBA7UkkGJxXZwVHWmExPn39E9VnT1af\n1Uk54FyqsrNOUkkJcPuUmzVz7JEusMoHRmuRwr5Nc5WcSwl3VAjzulMBujj6DWYY\nNc5CPL2/OQVPcXmToD0tYhZAOPgEQc48RUGVRYQlkt3jFMbhr9ayPD0CgYEAyvTd\nsD45oW1GlTXBzlHoFYH73W4nQjvhQXV8orY/AK9/ViKUJa14z9HMlCtUVbkq6gvD\nFkWu0b/1tHVGN0VY/Wf5+qaij9InvKZ2NcXNV1/Ni5zmueIU5qgyiZZxAUCMQbpy\nU57ESgtiODecEqXOYQPrIwCBdx+O8O6oQxJxgeUCgYAJxchOZP2z6OjrbNfXIvrI\nKK/VK1BiTWWhQ3eYhVBReJ5Eq5TaW5WoprW84XF/iBgCWmEtX8o1Jpj7RBsJl8ct\nsaUXVxw7yksEKinVJL7NsK/JSLR33SoirnamRBXZh1WkIilDJfXe5MG0Lfhj2hlA\nAyNKVqbmevNi8brpd5DBdQKBgGJNbh8SVJmisyBMVF3ZgD8CoXqkAvHqzPUGseKh\nwSxU1KlkwDrrpeuK47sUrZmDwYxxPAHKqJ1BjAHnF6ZnuW2r8gF8uppMoSCXxAPR\nld7vMUChM4PvRE5gQ3Iu4vdHS2f+pado7AwtLVqrXLYPh0GoQzjF4u9O4s5B2k/6\nW+4dAoGAWVzG1/TAahqxC4CH8nFe3Se3U50klaoVSOSTlNPgEcRancJVY/yXGO9+\nSa3NIJb/EivoEpNuUHPoojNklLRonVBzdvfpu2YSkpwgdGpfropwxFsnrGju635I\nFHq5xOYDtTxCUspH4WqN1BXrIHNgj6bP9tcsVbat16T34OAAteU=\n-----END RSA PRIVATE KEY-----\n'
+client_private_key   = serialization.load_pem_private_key(client_private_key_b, password=None)
 
 with socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM) as sock:
     sock.connect(("127.0.0.1", 4000))
     challenge =  frame.read(sock)
     print("Received challenge: ", challenge)
-    msg = b'world'
+    msg = client_private_key.sign(
+        challenge,
+        padding.PSS(
+            mgf=padding.MGF1(hashes.SHA256()),
+            salt_length=padding.PSS.MAX_LENGTH
+        ),
+        hashes.SHA256()
+    )
     print("Sending response: ", msg)
     frame.send(sock, msg)
     result = frame.read(sock)

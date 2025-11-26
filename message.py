6442bd9208203a1326a68478d51ba1d73ac62abd
Joakim Olsson <joakim.olsson@adicitus.cloud>
2025-11-26 16:21:14 +0100

    Added message format functionality

---
+++ message.py
@@ -0,0 +1,125 @@
+import uuid
+from datetime import datetime
+import enum
+
+class BackboneMessageFormat(enum.IntEnum):
+    C2C = 0
+    C2S = 1
+    S2S = 2
+
+class BackboneMessageType(enum.IntEnum):
+    pass
+
+class BackboneC2SType(BackboneMessageType):
+    HEARTBEAT = 0
+    STOP      = 15
+
+class BackboneS2SType(BackboneMessageType):
+    DONE  = 14
+    STOP  = 15
+
+# Base class for all Backbone messages:
+class BackboneMessage:
+    def __init__(self, format:BackboneMessageFormat, type:BackboneMessageType) -> None:
+        if format not in BackboneMessageFormat:
+            raise ValueError(f"Invalid Backbone message format: {format}")
+
+        self.format = format
+        self.type = type
+    
+    def to_bytes(self):
+        msg_typing = ((self.format << 4) + self.type).to_bytes(1)
+        return msg_typing
+    
+    def  __eq__(self, __o: object) -> bool:
+        if not isinstance(__o, BackboneMessage):
+            return False
+        return __o.format == self.format and __o.type == self.type
+    
+    @staticmethod
+    def from_bytes(frame:bytes):
+        t =  frame[0] & 0b00001111
+        f = (frame[0] & 0b11110000) >> 4
+
+        try:
+            f = BackboneMessageFormat(f)
+            match f:
+                case BackboneMessageFormat.C2C:
+                    # C2C messages don't have separate types, so this should be 0.
+                    if t != 0: return None # This is not a valid message frame
+                    recipient_id = uuid.UUID(bytes=frame[1:17])
+                    payload      = frame[17:]
+                    return BackboneMessageC2C(recipient_id, payload)
+
+                case BackboneMessageFormat.C2S:
+                    t = BackboneC2SType(t)
+                    timestamp = datetime.fromtimestamp(int.from_bytes(frame[1:5]))
+                    return BackboneMessageC2S(t, timestamp)
+
+                case BackboneMessageFormat.S2S:
+                    t = BackboneS2SType(t)
+                    timestamp = datetime.fromtimestamp(int.from_bytes(frame[1:5]))
+                    return BackboneMessageS2S(t, timestamp)
+                
+        except ValueError as e:
+            print(e)
+            return None
+
+
+class BackboneMessageC2C(BackboneMessage):
+    def __init__(self, recipient:uuid.UUID, payload:bytes) -> None:
+        super().__init__(BackboneMessageFormat.C2C, 0)
+        self.recipient = recipient
+        self.payload   = payload
+    
+    def to_bytes(self):
+        frame = super().to_bytes()
+        return frame + self.recipient.bytes + self.payload
+    
+    def __eq__(self, __o: object) -> bool:
+        if not isinstance(__o, BackboneMessageC2C):
+            return False
+            
+        return super().__eq__(__o) and __o.recipient == self.recipient and __o.payload == self.payload
+        
+
+class BackboneMessageC2S(BackboneMessage):
+    def __init__(self, type:BackboneC2SType, timestamp:datetime=None) -> None:
+        if type not in BackboneC2SType:
+            raise ValueError(f"Invalid Backbone C2S type: {type}")
+        super().__init__(BackboneMessageFormat.C2S, type)
+        if timestamp == None:
+            timestamp = datetime.now()
+        
+        self.timestamp = datetime.fromtimestamp(int(timestamp.timestamp()))
+    
+    def to_bytes(self):
+        frame = super().to_bytes()
+        return frame + int(self.timestamp.timestamp()).to_bytes(4)
+
+    def __eq__(self, __o: object) -> bool:
+        if not isinstance(__o, BackboneMessageC2S):
+            return False
+            
+        return super().__eq__(__o) and __o.timestamp == self.timestamp
+
+
+class BackboneMessageS2S(BackboneMessage):
+    def __init__(self, type:BackboneS2SType, timestamp:datetime=None) -> None:
+        if type not in BackboneS2SType:
+            raise ValueError(f"Invalid Backbone S2S type: {type}")
+        super().__init__(BackboneMessageFormat.S2S, type)
+        if timestamp == None:
+            timestamp = datetime.now()
+        
+        self.timestamp = datetime.fromtimestamp(int(timestamp.timestamp()))
+
+    def to_bytes(self):
+        frame = super().to_bytes()
+        return frame + int(self.timestamp.timestamp()).to_bytes(4)
+    
+    def __eq__(self, __o: object) -> bool:
+        if not isinstance(__o, BackboneMessageS2S):
+            return False
+            
+        return super().__eq__(__o) and __o.timestamp == self.timestamp
---
+++ message.tests.py
@@ -0,0 +1,108 @@
+from datetime import datetime
+from os import urandom
+from uuid import uuid4
+import unittest, uuid
+
+import message
+from message import BackboneMessage, BackboneMessageC2C, BackboneMessageC2S, BackboneMessageS2S, BackboneC2SType, BackboneS2SType, BackboneMessageFormat
+
+class TestMessageTranslation(unittest.TestCase):
+
+    def test_message_formats(self):
+        self.assertTrue(BackboneMessageFormat.C2C == 0)
+        self.assertTrue(BackboneMessageFormat.C2S == 1)
+        self.assertTrue(BackboneMessageFormat.S2S == 2)
+    
+    def test_message_c2s_types(self):
+        self.assertTrue(BackboneC2SType.HEARTBEAT == 0)
+        self.assertTrue(BackboneC2SType.STOP == 15)
+    
+    def test_message_s2s_types(self):
+        self.assertTrue(BackboneS2SType.DONE == 14)
+        self.assertTrue(BackboneS2SType.STOP == 15)
+        
+
+    def test_message_c2c_creation(self):
+        msg = BackboneMessageC2C(uuid4(), urandom(256))
+        self.assertIsInstance(msg, BackboneMessage)
+        self.assertIsInstance(msg, BackboneMessageC2C)
+    
+    def test_message_c2s_creation(self):
+        BackboneMessageC2S(BackboneC2SType.STOP)
+        BackboneMessageC2S(BackboneC2SType.HEARTBEAT)
+        BackboneMessageC2S(BackboneC2SType.STOP, datetime.now())
+        BackboneMessageC2S(BackboneC2SType.HEARTBEAT, datetime.now())
+        BackboneMessageC2S(15)
+        BackboneMessageC2S(0)
+        with self.assertRaises(ValueError):
+            msg = BackboneMessageC2S(-1)
+    
+    def test_message_s2s_creation(self):
+        BackboneMessageS2S(BackboneS2SType.STOP)
+        BackboneMessageS2S(BackboneS2SType.DONE)
+        BackboneMessageS2S(BackboneS2SType.STOP, datetime.now())
+        BackboneMessageS2S(BackboneS2SType.DONE, datetime.now())
+        BackboneMessageS2S(15)
+        BackboneMessageS2S(14)
+        with self.assertRaises(ValueError):
+            BackboneMessageS2S(-1)
+    
+    def test_message_c2c_equality(self):
+        recipient = uuid4()
+        payload   = urandom(256)
+        self.assertEqual(BackboneMessageC2C(recipient, payload), BackboneMessageC2C(recipient, payload))
+        self.assertNotEqual(BackboneMessageC2C(recipient, payload), BackboneMessageC2C(uuid4(), payload))
+        self.assertNotEqual(BackboneMessageC2C(recipient, payload), BackboneMessageC2C(recipient, urandom(256)))
+
+    def test_message_c2s_equality(self):
+        self.assertEqual(BackboneMessageC2S(BackboneC2SType.HEARTBEAT), BackboneMessageC2S(BackboneC2SType.HEARTBEAT))
+        self.assertEqual(BackboneMessageC2S(BackboneC2SType.STOP), BackboneMessageC2S(BackboneC2SType.STOP))
+        self.assertNotEqual(BackboneMessageC2S(BackboneC2SType.HEARTBEAT), BackboneMessageC2S(BackboneC2SType.STOP))
+        self.assertNotEqual(BackboneMessageC2S(BackboneC2SType.STOP), BackboneMessageC2S(BackboneC2SType.HEARTBEAT))
+    
+    def test_message_s2s_equality(self):
+        self.assertEqual(BackboneMessageS2S(BackboneS2SType.DONE), BackboneMessageS2S(BackboneS2SType.DONE))
+        self.assertEqual(BackboneMessageS2S(BackboneS2SType.STOP), BackboneMessageS2S(BackboneS2SType.STOP))
+    
+    def test_message_c2c_to_bytes(self):
+        recipient = uuid4()
+        payload   = urandom(16)
+        msg = BackboneMessageC2C(recipient, payload)
+        frame = msg.to_bytes()
+        self.assertEqual(frame, b'\x00' + recipient.bytes + payload)
+    
+    def test_message_c2s_to_bytes(self):
+        timestamp = datetime.now()
+        timestamp_b = int(timestamp.timestamp()).to_bytes(4)
+        msg = BackboneMessageC2S(BackboneC2SType.STOP, timestamp)
+        frame = msg.to_bytes()
+        self.assertEqual(frame, b'\x1F' + timestamp_b)
+
+    def test_message_s2s_to_bytes(self):
+        timestamp = datetime.now()
+        timestamp_b = int(timestamp.timestamp()).to_bytes(4)
+        msg = BackboneMessageS2S(BackboneS2SType.STOP, timestamp)
+        frame = msg.to_bytes()
+        self.assertEqual(frame, b'\x2F' + timestamp_b)
+
+    def test_message_c2c_from_bytes(self):
+        recipient = uuid4()
+        payload   = urandom(16)
+        msg = BackboneMessageC2C(recipient, payload)
+        self.assertEqual(msg, BackboneMessage.from_bytes(b'\x00' + recipient.bytes + payload))
+    
+    def test_message_c2s_from_bytes(self):
+        timestamp = datetime.now()
+        timestamp_b = int(timestamp.timestamp()).to_bytes(4)
+        msg = BackboneMessageC2S(BackboneC2SType.STOP, timestamp)
+        self.assertEqual(msg, BackboneMessage.from_bytes(b'\x1F' + timestamp_b))
+
+    def test_message_s2s_from_bytes(self):
+        timestamp = datetime.now()
+        timestamp_b = int(timestamp.timestamp()).to_bytes(4)
+        msg = BackboneMessageS2S(BackboneS2SType.STOP, timestamp)
+        self.assertEqual(msg, BackboneMessage.from_bytes(b'\x2F' + timestamp_b))
+
+
+if __name__ == "__main__":
+    unittest.main()
